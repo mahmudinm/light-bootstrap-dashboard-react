7e337f7bafe0ddac208a15fa45e778a172ff59dc
mahmudin muttaqin <claxmanx@gmail.com>
2020-03-02 12:32:41 +0700

    commit init

---
+++ src/layouts/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+
+class Home extends Component {
+  
+  render() {
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+}
+
+export default Home;
---
+++ src/views/Auth.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Auth extends Component {
+  render() {
+    return (
+      <div className="content">
+        <button className="btn btn-primary">Coba</button>
+      </div>
+    );
+  }
+}
+
+export default Auth;
